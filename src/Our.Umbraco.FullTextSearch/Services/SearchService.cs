c69098274c6277c7416aa2c4d2093a5c1c5bd3ca
Alexander Roode <alexroode@gmail.com>
2020-03-09 16:48:09 -0700

    Fix #4

--- src/Our.Umbraco.FullTextSearch/Services/SearchService.cs
+++ src/Our.Umbraco.FullTextSearch/Services/SearchService.cs
@@ -148,7 +148,8 @@ namespace Our.Umbraco.FullTextSearch.Services
                     query.AppendFormat(" AND ({0})", rootNodeGroup);
                 }
 
-                query.Append($" AND (__IndexType:content AND __Published_{_search.Culture}:y)");
+                var publishedPropertyName = string.IsNullOrEmpty(_search.Culture) ? "__Published" : $"__Published_{_search.Culture}";
+                query.Append($" AND (__IndexType:content AND {publishedPropertyName}:y)");
 
                 var searcher = index.GetSearcher();
                 _logger.Info<SearchService>("Trying to search for {query}", query.ToString());

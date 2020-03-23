26448ca1710634b402a2f298613605bcc7c76abf
Stefan Kip <stefan@wirelab.nl>
2020-03-23 14:59:17 +0100

    Fix #16 and some formatting stuff

--- src/Our.Umbraco.FullTextSearch/Services/SearchService.cs
+++ src/Our.Umbraco.FullTextSearch/Services/SearchService.cs
@@ -1,17 +1,16 @@
 ﻿using Examine;
-using Umbraco.Core.Logging;
+using Our.Umbraco.FullTextSearch.Helpers;
 using Our.Umbraco.FullTextSearch.Interfaces;
 using Our.Umbraco.FullTextSearch.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using Umbraco.Examine;
-using Umbraco.Web;
-using System;
-using Umbraco.Core;
-using Our.Umbraco.FullTextSearch.Helpers;
 using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
 
 namespace Our.Umbraco.FullTextSearch.Services
 {
@@ -22,7 +21,7 @@ namespace Our.Umbraco.FullTextSearch.Services
         private readonly ILogger _logger;
         private ISearch _search;
         private int _currentPage;
-        List<SearchProperty> _searchProperties;
+        private List<SearchProperty> _searchProperties;
 
         public SearchService(IExamineManager examineManager, IConfig fullTextConfig, ILogger logger)
         {
@@ -42,17 +41,17 @@ namespace Our.Umbraco.FullTextSearch.Services
             var results = GetResults();
 
             result.CurrentPage = currentPage;
-            result.TotalPages = search.PageLength > 0 && results.TotalItemCount > 0 ? (int)Math.Ceiling((decimal)results.TotalItemCount / (decimal)search.PageLength) : 1;
+            result.TotalPages = search.PageLength > 0 && results.TotalItemCount > 0 ? (int)Math.Ceiling(results.TotalItemCount / (decimal)search.PageLength) : 1;
             result.TotalResults = results.TotalItemCount;
-            result.Results = (search.PageLength > 0 ? results.Skip(search.PageLength * (currentPage - 1)) : results).Select(x => GetFullTextSearchResultItem(x));
+            result.Results = (search.PageLength > 0 ? results.Skip(search.PageLength * (currentPage - 1)) : results).Select(GetFullTextSearchResultItem);
 
             return result;
         }
 
         private void SetDefaults()
         {
-            if (!_search.BodyProperties.Any()) _search.BodyProperties = new string[] { _fullTextConfig.GetFullTextFieldName() };
-            if (!_search.TitleProperties.Any()) _search.TitleProperties = new string[] { _fullTextConfig.GetDefaultTitleFieldName() };
+            if (!_search.BodyProperties.Any()) _search.BodyProperties = new[] { _fullTextConfig.GetFullTextFieldName() };
+            if (!_search.TitleProperties.Any()) _search.TitleProperties = new[] { _fullTextConfig.GetDefaultTitleFieldName() };
             if (!_search.SummaryProperties.Any()) _search.SummaryProperties = _search.BodyProperties;
         }
 
@@ -61,7 +60,7 @@ namespace Our.Umbraco.FullTextSearch.Services
             _searchProperties = new List<SearchProperty>();
             var titleBoost = _fullTextConfig.GetSearchTitleBoost();
 
-            var bodyProperties = !_search.BodyProperties.Any() ? new string[] { _fullTextConfig.GetFullTextFieldName() } : _search.BodyProperties;
+            var bodyProperties = !_search.BodyProperties.Any() ? new[] { _fullTextConfig.GetFullTextFieldName() } : _search.BodyProperties;
 
             _searchProperties.AddRange(GetProperties(_search.TitleProperties, titleBoost, _search.Fuzzyness, _search.AddWildcard));
             _searchProperties.AddRange(GetProperties(bodyProperties, 1.0, _search.Fuzzyness, _search.AddWildcard));
@@ -80,11 +79,12 @@ namespace Our.Umbraco.FullTextSearch.Services
                 switch (_search.SearchType)
                 {
                     case SearchType.MultiRelevance:
+                    case SearchType.MultiAnd:
 
                         // We formulate the query differently depending on the input.
                         if (_search.SearchTerm.Contains('"'))
                         {
-                            // If the user has enetered double quotes we don't bother 
+                            // If the user has entered double quotes we don't bother
                             // searching for the full string
                             query.Append(QueryAllPropertiesOr(_search.SearchTermSplit, 1));
                         }
@@ -95,38 +95,11 @@ namespace Our.Umbraco.FullTextSearch.Services
                         }
                         else
                         {
-                            // otherwise we search first for the entire query in quotes, 
+                            // otherwise we search first for the entire query in quotes,
                             // then for each term in the query OR'd together.
-                            query.AppendFormat("({0} OR {1})",
-                                QueryAllPropertiesOr(_search.SearchTermQuoted, 2)
-                                , QueryAllPropertiesOr(_search.SearchTermSplit, 1)
-                            );
+                            query.Append($"({QueryAllPropertiesOr(_search.SearchTermQuoted, 2)} OR {QueryAllPropertiesOr(_search.SearchTermSplit, 1)})");
                         }
 
-                        break;
-
-                    case SearchType.MultiAnd:
-
-                        if (_search.SearchTerm.Contains('"'))
-                        {
-                            // If the user has enetered double quotes we don't bother 
-                            // searching for the full string
-                            query.Append(QueryAllPropertiesAnd(_search.SearchTermSplit, 1.0));
-                        }
-                        else if (!_search.SearchTerm.Contains('"') && !_search.SearchTerm.Contains(' '))
-                        {
-                            // if there's no spaces or quotes we don't need to get the quoted term and boost it
-                            query.Append(QueryAllPropertiesAnd(_search.SearchTermSplit, 1));
-                        }
-                        else
-                        {
-                            // otherwise we search first for the entire query in quotes, 
-                            // then for each term in the query OR'd together.
-                            query.AppendFormat("{0} OR {1}",
-                                QueryAllPropertiesAnd(_search.SearchTermQuoted, 2)
-                                , QueryAllPropertiesAnd(_search.SearchTermSplit, 1)
-                            );
-                        }
                         break;
 
                     case SearchType.SimpleOr:
@@ -144,8 +117,9 @@ namespace Our.Umbraco.FullTextSearch.Services
                 if (_search.RootNodeIds.Any())
                 {
                     var pathName = _fullTextConfig.GetPathFieldName();
-                    var rootNodeGroup = string.Join(" OR ", _search.RootNodeIds.Select(x => string.Format("{0}:{1}", pathName, x.ToString())));
-                    query.AppendFormat(" AND ({0})", rootNodeGroup);
+                    var rootNodeGroup = string.Join(" OR ", _search.RootNodeIds.Select(x =>
+                        $"{pathName}:{x}"));
+                    query.Append($" AND ({rootNodeGroup})");
                 }
 
                 query.Append($" AND (__IndexType:content AND __Published_{_search.Culture}:y)");
@@ -161,7 +135,7 @@ namespace Our.Umbraco.FullTextSearch.Services
         /// <summary>
         /// OR's together all the passed search terms into a query
         /// for each property in the properties list
-        /// 
+        ///
         /// </summary>
         /// <param name="searchTerms">A list of fully escaped search terms</param>
         /// <param name="boostAll">all terms are boosted by this amount, multiplied by the amount in the property/boost dictionary</param>
@@ -173,6 +147,7 @@ namespace Our.Umbraco.FullTextSearch.Services
 
             return QueryAllProperties(searchTerms, boostAll, "OR");
         }
+
         /// <summary>
         /// AND's together all the passed search terms into a query
         /// for each property in the properties list
@@ -191,13 +166,13 @@ namespace Our.Umbraco.FullTextSearch.Services
         /// <summary>
         /// Called by queryAllPropertiesOr, queryAllPropertiesAnd
         /// Creates a somewhat convuleted lucene query string.
-        /// Each search term is applied to each property in the umbracoProperties list, 
+        /// Each search term is applied to each property in the umbracoProperties list,
         /// boosted by the boost value associated with the property, multiplied by
-        /// the boost value passed to the function. 
-        /// The global fuzziness level is applied, multiplied by the fuzzyness value 
+        /// the boost value passed to the function.
+        /// The global fuzziness level is applied, multiplied by the fuzzyness value
         /// associated with the relevant property.
         /// Terms are ether OR'd or AND'd (or theoretically anything else
-        /// you stick into joinWith'd, though I can't think of much that would 
+        /// you stick into joinWith'd, though I can't think of much that would
         /// actually be useful) according to the contents of joinWith
         /// </summary>
         /// <param name="searchTerms">A list of fully escaped search terms</param>
@@ -227,10 +202,10 @@ namespace Our.Umbraco.FullTextSearch.Services
             var i = 0;
             for (; ; )
             {
-                query.AppendFormat(" ({0}) ", queryBuilder[i]);
+                query.Append($" ({queryBuilder[i]}) ");
                 if (++i >= count)
                     break;
-                query.AppendFormat("{0} ", joinWith);
+                query.Append($"{joinWith} ");
             }
             return query;
         }
@@ -240,7 +215,7 @@ namespace Our.Umbraco.FullTextSearch.Services
             var boostString = string.Empty;
             if (boost != 1.0)
             {
-                boostString = "^" + boost;
+                boostString = "^" + boost.ToString(CultureInfo.InvariantCulture);
             }
             var fuzzyString = string.Empty;
             var wildcardQuery = string.Empty;
@@ -249,18 +224,19 @@ namespace Our.Umbraco.FullTextSearch.Services
                 // wildcard queries get lower relevance than exact matches, and ignore fuzzieness
                 if (property.Wildcard)
                 {
-                    wildcardQuery = string.Format(System.Globalization.CultureInfo.InvariantCulture, "({0}:{1}*^{2} OR {0}_{4}:{1}*^{2}) ", property.PropertyName, term, boost * 0.5, _search.Culture);
+                    var halfBoost = (boost * 0.5).ToString(CultureInfo.InvariantCulture);
+                    wildcardQuery = $"({property.PropertyName}:{term}*^{halfBoost} OR {property.PropertyName}_{_search.Culture}:{term}*^{halfBoost}) ";
                 }
                 else
                 {
-                    double fuzzyLocal = property.FuzzyMultiplier;
+                    var fuzzyLocal = property.FuzzyMultiplier;
                     if (fuzzyLocal < 1.0 && fuzzyLocal > 0.0)
                     {
                         fuzzyString = "~" + fuzzyLocal;
                     }
                 }
             }
-            return string.Format(System.Globalization.CultureInfo.InvariantCulture, "({0}:{1}{2}{3} OR {0}_{5}:{1}{2}{3}) {4}", property.PropertyName, term, fuzzyString, boostString, wildcardQuery, _search.Culture);
+            return $"({property.PropertyName}:{term}{fuzzyString}{boostString} OR {property.PropertyName}_{_search.Culture}:{term}{fuzzyString}{boostString}) {wildcardQuery}";
         }
         protected string QuerySingleItemSimple(string term, SearchProperty property)
         {
@@ -281,29 +257,24 @@ namespace Our.Umbraco.FullTextSearch.Services
                     }
                 }
             }
-            return string.Format(System.Globalization.CultureInfo.InvariantCulture, "({0}:{1}{2}{3} OR {0}_{4}:{1}{2}{3}) ", property.PropertyName, term, fuzzyString, wildcard, _search.Culture);
+            return $"({property.PropertyName}:{term}{fuzzyString}{wildcard} OR {property.PropertyName}_{_search.Culture}:{term}{fuzzyString}{wildcard}) ";
         }
 
         /// <summary>
-        /// Split up the comma separated string and retun a list of UmbracoProperty objects
+        /// Split up the comma separated string and return a list of UmbracoProperty objects
         /// </summary>
-        /// <param name="commaSeparated"></param>
+        /// <param name="properties"></param>
         /// <param name="boost"></param>
         /// <param name="fuzzy"></param>
         /// <param name="wildcard"></param>
         /// <returns></returns>
-        static List<SearchProperty> GetProperties(string[] properties, double boost, double fuzzy, bool wildcard)
+        private static IEnumerable<SearchProperty> GetProperties(string[] properties, double boost, double fuzzy, bool wildcard)
         {
             var umbracoProperties = new List<SearchProperty>();
             if (properties.Any())
             {
-                foreach (var propName in properties)
-                {
-                    if (!string.IsNullOrEmpty(propName))
-                    {
-                        umbracoProperties.Add(new SearchProperty(propName, boost, fuzzy, wildcard));
-                    }
-                }
+                umbracoProperties.AddRange(properties.Where(propName => !string.IsNullOrEmpty(propName))
+                    .Select(propName => new SearchProperty(propName, boost, fuzzy, wildcard)));
             }
             return umbracoProperties;
         }
@@ -325,7 +296,7 @@ namespace Our.Umbraco.FullTextSearch.Services
         public string GetTitle(ISearchResult result)
         {
             var title = string.Empty;
-            var props = _search.TitleProperties.SelectMany(x => new string[] { $"{x}_{_search.Culture}", x });
+            var props = _search.TitleProperties.SelectMany(x => new[] { $"{x}_{_search.Culture}", x });
             foreach (var prop in props)
             {
                 title = result.GetValues(prop).FirstOrDefault();
@@ -337,12 +308,12 @@ namespace Our.Umbraco.FullTextSearch.Services
         public string GetSummary(ISearchResult result)
         {
             var summary = string.Empty;
-            var props = _search.SummaryProperties.Concat(_search.BodyProperties).SelectMany(x => new string[] { $"{x}_{_search.Culture}", x });
+            var props = _search.SummaryProperties.Concat(_search.BodyProperties).SelectMany(x => new[] { $"{x}_{_search.Culture}", x });
             foreach (var prop in props)
             {
                 summary = result.GetValues(prop).FirstOrDefault();
                 if (summary.IsNullOrWhiteSpace()) continue;
-                
+
                 summary = SummarizeText(summary);
                 break;
             }
@@ -352,11 +323,11 @@ namespace Our.Umbraco.FullTextSearch.Services
         protected string SummarizeText(string input)
         {
             var summaryBuilder = new StringBuilder();
-            var summary = "";
+            string summary;
             if (!input.IsNullOrWhiteSpace() && (input.Length > _search.SummaryLength || _search.HighlightSearchTerms))
             {
                 //(\\S*.{0,10})?("+ queryString +")(.{0,10}\\S*)?
-                var searchTerm = "";
+                string searchTerm;
                 if (_search.SearchTerm.Contains('"'))
                 {
                     searchTerm = string.Join("|", _search.SearchTermSplit);
@@ -383,8 +354,9 @@ namespace Our.Umbraco.FullTextSearch.Services
                         {
                             if (match.Index > 0 && !match.Groups[1].Value.IsNullOrWhiteSpace()) summaryBuilder.Append($" &hellip;{match.Groups[1].Value}");
 
-                            if (_search.HighlightSearchTerms) summaryBuilder.Append($"<b>{match.Groups[2].Value}</b>");
-                            else summaryBuilder.Append(match.Groups[2].Value);
+                            summaryBuilder.Append(_search.HighlightSearchTerms
+                                ? $"<b>{match.Groups[2].Value}</b>"
+                                : match.Groups[2].Value);
 
                             if (!match.Groups[3].Value.IsNullOrWhiteSpace()) summaryBuilder.Append($"{match.Groups[3].Value}&hellip; ");
                         }

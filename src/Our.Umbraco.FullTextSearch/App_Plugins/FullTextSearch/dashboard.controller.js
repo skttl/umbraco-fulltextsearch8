b7974f378bdc78ab491507c82b4589f52779e738
Søren Kottal <sk@ecreo.dk>
2020-10-14 11:02:08 +0200

    dashboard tools and tweaks

--- src/Our.Umbraco.FullTextSearch/App_Plugins/FullTextSearch/dashboard.controller.js
+++ src/Our.Umbraco.FullTextSearch/App_Plugins/FullTextSearch/dashboard.controller.js
@@ -30,11 +30,46 @@
              * Cache handling
              */
             vm.rebuildCache = {
+                includeDescendants: false,
+                toggleIncludeDescendants: function () {
+                    vm.rebuildCache.includeDescendants = !vm.rebuildCache.includeDescendants;
+                },
+                pickNodes: false,
+                pickedNodes:
+                {
+                    label: 'Nodes',
+                    hideLabel: true,
+                    editor: "Umbraco.MultiNodeTreePicker",
+                    view: 'contentPicker',
+                    config: {
+                        ignoreUserStartNodes: false,
+                        maxNumber: 0,
+                        minNumber: 0,
+                        multiPicker: true,
+                        showOpenButton: false,
+                        startNode: null
+                    },
+                    value: ""
+                },
+                togglePickNodes: function () {
+                    vm.rebuildCache.includeDescendants = false;
+                    if (vm.rebuildCache.pickNodes) {
+                        vm.rebuildCache.pickedNodes.value = "";
+                        vm.rebuildCache.pickNodes = false;
+                    }
+                    else {
+                        vm.rebuildCache.pickNodes = true;
+                    }
+                },
                 buttonState: "init",
                 rebuild: function () {
                     vm.rebuildCache.buttonState = "busy";
+                    var nodeIds = vm.rebuildCache.pickNodes ? vm.rebuildCache.pickedNodes.value : "*";
+                    if (vm.rebuildCache.includeDescendants && nodeIds != "*") {
+                        nodeIds += "&includeDescendants=true"
+                    }
 
-                    $http.post('/umbraco/backoffice/FullTextSearch/Index/RebuildCache?nodeIds=*').then(
+                    $http.post('/umbraco/backoffice/FullTextSearch/Index/RebuildCache?nodeIds=' + nodeIds).then(
                         function (response) {
                             vm.rebuildCache.buttonState = "success";
                         }

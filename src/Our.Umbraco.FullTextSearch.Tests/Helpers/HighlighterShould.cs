1d4ba1382008d380cee16708c0c90bd19f055a0c
Markus Johansson <markus@obviuse.se>
2023-06-21 13:49:15 +0200

    Added unit test project and highlighter tests

---
+++ src/Our.Umbraco.FullTextSearch.Tests/Helpers/HighlighterShould.cs
@@ -0,0 +1,24 @@
+﻿using Our.Umbraco.FullTextSearch.Helpers;
+
+namespace Our.Umbraco.FullTextSearch.Tests.Helpers;
+
+public class HighlighterShould
+{
+    [Test]
+    public void Highlight_Exact_Matches()
+    {
+        var test = Highlighter.FindSnippet("This token should be highlighted, but not al tokens will be. Just that token in the beginning.", "token", 200, "<b>{0}</b>");
+
+        Assert.That(test, Is.EqualTo("This <b>token</b> should be highlighted, but not al tokens will be. Just that <b>token</b> in the beginning"));
+
+    }
+
+    [Test]
+    public void FindSnippet_And_Truncate()
+    {
+        var test = Highlighter.FindSnippet("This token should be highlighted, but not al tokens will be. Just that token in the beginning.", "token", 30, "<b>{0}</b>");
+
+        Assert.That(test, Is.EqualTo(" Just that <b>token</b> in the beginn"));
+
+    }
+}
